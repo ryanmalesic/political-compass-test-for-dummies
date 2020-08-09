cd2514290853d41dbe73fdd745f49b50454a0505
Ryan Malesic <ryanmalesic@icloud.com>
2020-08-09 15:24:14 -0400

    Add political compass test logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,213 +1,126 @@
+import React from 'react'
 import Head from 'next/head'
 
-export const Home = (): JSX.Element => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+import styles from './index.module.scss'
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+enum Quadrent {
+  AUTH_LEFT = 'AuthLeft',
+  AUTH_RIGHT = 'AuthRight',
+  LIB_LEFT = 'LibLeft',
+  LIB_RIGHT = 'LibRight',
+}
 
-      <p className="description">
-        Get started by editing <code>pages/index.tsx</code>
-      </p>
+export const Home = (): JSX.Element => {
+  const [equal, setEqual] = React.useState<boolean>(false)
+  const [enforce, setEnforce] = React.useState<boolean>(false)
+  const [quadrant, setQuadrant] = React.useState<Quadrent | null>(null)
 
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest')
-        }}
-      >
-        Test Button
-      </button>
+  const equalYes = equal ? 'is-selected is-success' : ''
+  const equalNo = !equal ? 'is-selected is-danger' : ''
+  const enforceYes = enforce ? 'is-selected is-success' : ''
+  const enforceNo = !enforce ? 'is-selected is-danger' : ''
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  const determineQuadrant = () => {
+    if (equal && enforce) {
+      setQuadrant(Quadrent.AUTH_LEFT)
+    } else if (!equal && enforce) {
+      setQuadrant(Quadrent.AUTH_RIGHT)
+    } else if (equal && !enforce) {
+      setQuadrant(Quadrent.LIB_LEFT)
+    } else {
+      setQuadrant(Quadrent.LIB_RIGHT)
+    }
+  }
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+  return (
+    <div className={styles.container}>
+      {quadrant ? (
+        <section className="section">
+          <div className="contatiner has-text-centered">
+            <p className="title is-1"> You are a {quadrant.toString()}</p>
+          </div>
+        </section>
+      ) : (
+        <div className={styles.container}>
+          <Head>
+            <title>Political Compass Test For Dummies</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+          <section className="section">
+            <div className="contatiner has-text-centered">
+              <p className="title is-1">Political Compass Test For Dummies</p>
+              <p className="subtitle is-3">
+                Find out which quadrant you belong in
+              </p>
+            </div>
+          </section>
 
-        <a
-          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
-    </main>
+          <section className="section">
+            <div className="container has-text-centered">
+              <div className="card mb-2">
+                <header className="card-header ">
+                  <p className="card-header-title is-centered">
+                    Do you believe that everyone is equal?
+                  </p>
+                </header>
+                <div className="card-content">
+                  <div className="content">
+                    <div className="buttons has-addons is-centered">
+                      <button
+                        onClick={() => setEqual(false)}
+                        className={`button ${equalNo}`}
+                      >
+                        No
+                      </button>
+                      <button
+                        onClick={() => setEqual(true)}
+                        className={`button ${equalYes}`}
+                      >
+                        Yes
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      .logo {
-        height: 1em;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
+              <div className="card mb-2">
+                <header className="card-header">
+                  <p className="card-header-title is-centered">
+                    Do you believe that the government should enforce it?
+                  </p>
+                </header>
+                <div className="card-content">
+                  <div className="content">
+                    <div className="buttons has-addons is-centered">
+                      <button
+                        onClick={() => setEnforce(false)}
+                        className={`button ${enforceNo}`}
+                      >
+                        No
+                      </button>
+                      <button
+                        onClick={() => setEnforce(true)}
+                        className={`button ${enforceYes}`}
+                      >
+                        Yes
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button
+                onClick={determineQuadrant}
+                className="button is-fullwidth"
+              >
+                Go
+              </button>
+            </div>
+          </section>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Home
